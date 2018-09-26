345bd3f05ce4bd6c3d5556851a81a6cea01f0f8e
shimamama <tsmahkar@gmail.com>
2018-09-26 10:06:28 +0900

    フォント
    ネットから拾ったフォントを追加して、タイトル画面のTextViewとButtonの文字を変更。

--- app/src/main/java/com/test/color/MainActivity.java
+++ app/src/main/java/com/test/color/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.test.color.MainActivity;
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txt = (TextView) findViewById(R.id.title_text);
+        Typeface font = Typeface.createFromAsset(getAssets(), "shirokuma.otf");
+        txt.setTypeface(font);
+
+        Button txt2 = (Button) findViewById(R.id.title_start);
+        Typeface font2 = Typeface.createFromAsset(getAssets(),"shirokuma.otf");
+        txt2.setTypeface(font2);
     }
 
     // 画面切り替え  **********************************
